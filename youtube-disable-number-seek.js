c7e2cae3c44ef6d9c6e6f3ed851a402d586c0449
Tuure <tuure.piitulainen@gmail.com>
2024-06-12 04:28:27 +0300

    Add settings to toggle 0, Home and End
    The checkboxes are located in Manage Extension -> Preferences

--- youtube-disable-number-seek.js
+++ youtube-disable-number-seek.js
@@ -6,6 +6,24 @@
  * https://github.com/ccampbell/mousetrap/blob/master/mousetrap.js
  */
 
+const settings = {
+    disable_0: true,
+    disable_home: true,
+    disable_end: true,
+};
+
+const keys = Object.keys(settings);
+
+browser.storage.local.get(keys).then(result => {
+    for (const key of keys) {
+        if (result[key] !== undefined) {
+            settings[key] = result[key];
+        } else {
+            browser.storage.local.set({[key]: true});
+        }
+    }
+});
+
 function keyboard_event_handler(e) {
     // Don't prevent entering numbers, Home/End in input areas
     if (e.target.tagName == 'INPUT' ||
@@ -14,11 +32,14 @@ function keyboard_event_handler(e) {
         e.target.isContentEditable) {
         return;
     }
+
     // Trap number keys and Home/End keys
-    if ((e.code >= 'Digit0' && e.code <= 'Digit9') || 
-        (e.code >= 'Numpad0' && e.code <= 'Numpad9') ||
-        e.code === 'Home' || 
-        e.code === 'End') {
+    if ((e.code >= 'Digit1' && e.code <= 'Digit9') || 
+        (e.code >= 'Numpad1' && e.code <= 'Numpad9') ||
+        (settings.disable_0 && e.code === 'Digit0') ||
+        (settings.disable_0 && e.code === 'Numpad0') ||
+        (settings.disable_home && e.code === 'Home') || 
+        (settings.disable_end && e.code === 'End')) {
         e.stopImmediatePropagation();
     }
 }
